a2a59b8d05b1030d5fd7f8edf6c013117138c42f
Dmytro <2dmitri.akimov@gmail.com>
2023-10-03 21:27:00 +0200

    useState-costForm

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,20 +1,47 @@
+import { useState } from 'react';
 import './CostForm.css';
 
 const CostForm = () => {
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value);
+    };
+
     return (
         <form>
             <div className="new-cost__controls">
                 <div className="new-cost__control">
-                    <label>Title</label>
-                    <input type="text" />
+                    <label>Name</label>
+                    <input type="text" onChange={nameChangeHandler} />
                 </div>
                 <div className="new-cost__control">
                     <label>Sum</label>
-                    <input type="number" nim="0.01" step="0.01" />
+                    <input
+                        type="number"
+                        onChange={amountChangeHandler}
+                        nim="0.01"
+                        step="0.01"
+                    />
                 </div>
                 <div className="new-cost__control">
-                    <label>Sum</label>
-                    <input type="date" nim="2023-01-01" step="2023-12-31" />
+                    <label>Date</label>
+                    <input
+                        type="date"
+                        onChange={dateChangeHandler}
+                        nim="2023-01-01"
+                        step="2023-12-31"
+                    />
                 </div>
                 <div className="new-cost__actions">
                     <button type="submit">Add expense</button>
