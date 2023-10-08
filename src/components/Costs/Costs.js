035270fea08ff94110361bd89474960ed9f5236d
Dmytro <2dmitri.akimov@gmail.com>
2023-10-08 11:08:15 +0200

    NoExpensesThisYear

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -5,22 +5,24 @@ import './Costs.css';
 import { useState } from 'react';
 
 function Costs(props) {
-    const [selectedYear, setSelectedYear] = useState('');
+    const [selectedYear, setSelectedYear] = useState('2023');
 
     const onChangeYear = (year) => {
         setSelectedYear(year);
     };
 
+    const filteredCosts = props.costs.filter((cost) => {
+        return cost.date.getFullYear().toString() === selectedYear;
+    });
+
     return (
         <div>
             <Card className="costs">
                 <CostsFilter onChangeYear={onChangeYear} year={selectedYear} />
-                {props.costs
-                    .filter(
-                        (item) =>
-                            selectedYear === item.date.getFullYear().toString()
-                    )
-                    .map((item) => {
+                {filteredCosts.length === 0 ? (
+                    <p>No expenses this year</p>
+                ) : (
+                    filteredCosts.map((item) => {
                         return (
                             <CostItem
                                 key={item.id}
@@ -29,7 +31,8 @@ function Costs(props) {
                                 amount={item.amount}
                             />
                         );
-                    })}
+                    })
+                )}
             </Card>
         </div>
     );
